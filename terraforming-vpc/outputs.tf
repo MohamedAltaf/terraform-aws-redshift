c5a72e1c34e1ce6236c91c9fd77562c7ccc2e4ed
MohamedAltaf <basheer.altaf@gmail.com>
2019-04-09 14:27:40 -0500

    Removed vpn gate way and dchp template

--- terraforming-vpc/outputs.tf
+++ terraforming-vpc/outputs.tf
@@ -6,14 +6,6 @@ output "private_route_table_id" {
   value = "${aws_route_table.private_route_table.id}"
 }
 
-output "vpn_gateway_id" {
-  value = "${aws_vpn_gateway.vpn_gw.id}"
-}
-
-output "dhcp_template_id" {
-  value = "${aws_vpc_dhcp_options.citi_dhcp_template.id}"
-}
-
 output "s3_private_endpoint_id" {
   value = "${aws_vpc_endpoint.s3_endpoint.id}"
 }
