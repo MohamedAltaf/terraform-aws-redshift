4d317d8caf83f0fd7c164b152f8609c4307d9ef7
MohamedAltaf <basheer.altaf@gmail.com>
2019-04-09 14:27:19 -0500

    Removed unwanted variables

--- terraforming-vpc/variables.tf
+++ terraforming-vpc/variables.tf
@@ -16,31 +16,7 @@ variable "availability_zones" {
 
 variable "vpc_cidr" {
   type    = "string"
-  default = "10.119.26.0/23"
-}
-
-variable "AppID" {
-  description = "What is the CSI App ID that will be located in this VPC?"
-  type = "string"
-}
-
-variable "AppName" {
-  description = "Application user friendly name - (no sapce and 10 characters max)"
-  type = "string"
-}
-
-variable "BillingID" {
-  description = "What is the CSI Biling Profile number to be used with the App ID that will be located in this VPC?"
-  type = "string"
-}
-
-variable "amazon_side_asn" {
-  description = "The Autonomous System Number (ASN) for the Amazon side of the gateway. By default the virtual private gateway is created with the current default Amazon ASN."
-}
-
-variable "domain_name_servers" {
-  description = "Specify a list of DNS server addresses for DHCP options set, default to AWS provided"
-  type = "list"
+  default = "10.0.0.0/16"
 }
 
 variable "tags" {
