109436bd2ecea660d00ae892a85f82b517364962
MohamedAltaf <basheer.altaf@gmail.com>
2019-04-10 12:27:02 -0500

    Added all the cluster related variables in variable.tf

--- terraforming-redshift/variables.tf
+++ terraforming-redshift/variables.tf
@@ -21,6 +21,26 @@ variable "redshift_subnets" {
   default     = []
 }
 
+variable "cluster_identifier" {
+  description = "Custom name of the cluster"
+  default     = "redshift-cluster"
+}
+
+variable "cluster_node_type" {
+  description = "Node Type of Redshift cluster"
+  default     = "dc1.large"
+}
+
+variable "cluster_number_of_nodes" {
+  description = "Number of nodes in the cluster (values greater than 1 will trigger 'cluster_type' of 'multi-node')"
+  default     = 2
+}
+
+variable "cluster_database_name" {
+  description = "The name of the database to create"
+  default     = "dev"
+}
+
 variable "cluster_master_username" {}
 
 variable "cluster_master_password" {}
