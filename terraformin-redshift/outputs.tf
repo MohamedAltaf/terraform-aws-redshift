521fc0dfdbe3d82c0b5b2fa43dbb58cfb36a0f2d
MohamedAltaf <basheer.altaf@gmail.com>
2019-04-10 01:32:27 -0500

    Included the RedShift endpoint and S3 bucket SRN in the output file

---
+++ terraformin-redshift/outputs.tf
@@ -0,0 +1,13 @@
+output "iaas" {
+  value = "aws"
+}
+
+output "redshift_cluster_endpoint" {
+  description = "The connection endpoint"
+  value       = "${module.redshift.redshift_cluster_endpoint}"
+}
+
+output "bucket" {
+  description = S3 bucket arn
+  value = "${module.redshift.bucket}"
+}
