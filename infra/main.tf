1b4e8c53c7d2820bdc56f606c0fafdc6c8a84085
MohamedAltaf <basheer.altaf@gmail.com>
2019-04-10 00:35:45 -0500

    Removed vms security group

--- infra/main.tf
+++ infra/main.tf
@@ -6,29 +6,7 @@ resource "aws_vpc" "vpc" {
   tags = "${merge(var.tags, map("Name", "${var.env_name}-vpc"))}"
 }
 
-resource "aws_security_group" "vms_security_group" {
-  name        = "vms_security_group"
-  description = "VMs Security Group"
-  vpc_id      = "${aws_vpc.vpc.id}"
-
-  ingress {
-    cidr_blocks = ["${var.vpc_cidr}"]
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-  }
-
-  egress {
-    cidr_blocks = ["0.0.0.0/0"]
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-  }
-
-  tags = "${merge(var.tags, map("Name", "${var.env_name}-vms-security-group"))}"
-}
-
-# Private Subnet ===============================================================
+# Private Route Table ===============================================================
 
 resource "aws_route_table" "private_route_table" {
   vpc_id = "${aws_vpc.vpc.id}"
