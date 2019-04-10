7e9ea0a047cc3959cd5072e8f202a0ad649e07f9
MohamedAltaf <basheer.altaf@gmail.com>
2019-04-10 12:47:34 -0500

    Given the prooer value for s3 bucket arn

--- redshift/iam.tf
+++ redshift/iam.tf
@@ -15,7 +15,7 @@ resource "aws_iam_role" "redshift_role" {
         "Effect": "Allow",
         "Action": ["s3:ListBucket"],
         "Resource": [
-          "${redshift_bucket_arn}"
+          "${aws_s3_bucket.redshift_bucket.arn}"
         ]
       },
       {
@@ -26,7 +26,7 @@ resource "aws_iam_role" "redshift_role" {
           "s3:DeleteObject"
         ],
         "Resource": [
-          "${redshift_bucket_arn}/*"
+          "${aws_s3_bucket.redshift_bucket.arn}/*"
         ]
       }
     ]
