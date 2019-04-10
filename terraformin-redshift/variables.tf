257a4e1d48b41c7bb6a37de844e0a28e682432f9
MohamedAltaf <basheer.altaf@gmail.com>
2019-04-10 01:32:06 -0500

    Added all the required variables

---
+++ terraformin-redshift/variables.tf
@@ -0,0 +1,26 @@
+variable "env_name" {}
+
+variable "access_key" {}
+
+variable "secret_key" {}
+
+variable "region" {}
+
+variable "availability_zones" {
+  type = "list"
+}
+
+variable "vpc_cidr" {
+  type    = "string"
+  default = ""
+}
+
+variable "cluster_master_username" {}
+
+variable "cluster_master_password" {}
+
+variable "tags" {
+  type        = "map"
+  default     = {}
+  description = "Key/value tags to assign to all AWS resources"
+}
