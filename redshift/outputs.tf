b73fdce2d5efc74bcffe3a4bb28c1f67e103a126
MohamedAltaf <basheer.altaf@gmail.com>
2019-04-10 12:20:53 -0500

    Removed cluster related outputs

--- redshift/outputs.tf
+++ redshift/outputs.tf
@@ -1,28 +1,3 @@
-output "redshift_cluster_identifier" {
-  description = "The Redshift cluster identifier"
-  value       = "${aws_redshift_cluster.redshift.cluster_identifier}"
-}
-
-output "redshift_cluster_type" {
-  description = "The Redshift cluster type"
-  value       = "${aws_redshift_cluster.redshift.cluster_type}"
-}
-
-output "redshift_cluster_node_type" {
-  description = "The type of nodes in the cluster"
-  value       = "${aws_redshift_cluster.redshift.node_type}"
-}
-
-output "redshift_cluster_number_of_nodes" {
-  description = "The Redshift number of nodes"
-  value       = "${aws_redshift_cluster.redshift.number_of_nodes}"
-}
-
-output "redshift_cluster_database_name" {
-  description = "The name of the default database in the Cluster"
-  value       = "${aws_redshift_cluster.redshift.database_name}"
-}
-
 output "redshift_subnets" {
   description = "List of IDs of redshift subnets"
   value       = ["${aws_subnet.redshift_subnets.*.id}"]
